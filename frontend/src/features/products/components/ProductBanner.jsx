0e9fc7c8c6863297294cdf0c5bb3a24ef2a2bfc8
garrettwilloughby <garrettwilloughby@gmail.com>
2026-06-04 09:52:35 -0400

    fix: install swiper and replace previous library

--- frontend/src/features/products/components/ProductBanner.jsx
+++ frontend/src/features/products/components/ProductBanner.jsx
@@ -1,50 +1,44 @@
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
 import MobileStepper from '@mui/material/MobileStepper';
 import { Box, useTheme } from '@mui/material';
 import { useState } from 'react';
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
-export const ProductBanner = ({images}) => {
-
-    const theme=useTheme()
-
+export const ProductBanner = ({ images }) => {
+    const theme = useTheme();
     const [activeStep, setActiveStep] = useState(0);
-    const maxSteps = images.length;
 
-    const handleNext = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
+    return (
+        <>
+            <Swiper
+                modules={[Autoplay]}
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                direction="horizontal"
+                reverseDirection={theme.direction === 'rtl'}
+                initialSlide={0}
+                onSlideChange={(s) => setActiveStep(s.activeIndex)}
+                style={{ width: '100%', height: '100%', overflow: 'hidden' }}
+            >
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <Box
+                            component="img"
+                            sx={{ width: '100%', objectFit: 'contain' }}
+                            src={image}
+                            alt="Banner Image"
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
 
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
-    const handleStepChange = (step) => {
-        setActiveStep(step);
-    };
-
-  return (
-    <>
-    <AutoPlaySwipeableViews style={{overflow:"hidden"}} width={'100%'} height={'100%'} axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'} index={activeStep} onChangeIndex={handleStepChange} enableMouseEvents >
-        {
-        images.map((image,index) => (
-        <div key={index} style={{width:"100%",height:'100%'}}>
-            {
-            Math.abs(activeStep - index) <= 2 
-                ?
-                <Box component="img" sx={{width:'100%',objectFit:"contain"}} src={image} alt={'Banner Image'} />
-                :
-                    null
-            }
-        </div>
-        ))
-        }
-    </AutoPlaySwipeableViews>
-    <div style={{alignSelf:'center'}}>
-        <MobileStepper steps={maxSteps} position="static" activeStep={activeStep}/>
-    </div>
-    </>
-  )
-}
+            <div style={{ alignSelf: 'center' }}>
+                <MobileStepper
+                    steps={images.length}
+                    position="static"
+                    activeStep={activeStep}
+                />
+            </div>
+        </>
+    );
+};
